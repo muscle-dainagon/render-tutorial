fc184cf19a142ec555267e72fdf00576c9c3ab4a
isami <muscle.dainagon133@gmail.com>
2024-12-01 15:51:46 +0900

    test timezone

--- counter/views.py
+++ counter/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
 
 
 def index(request):
     """トップページのビュー"""
+    print("localtime: ", timezone.localtime().date())
+    print("timezone: ", timezone.now().date())
     return render(request, "counter/index.html")
